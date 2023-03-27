3086c35dcd713a9ba3c711819c4601b322dfdfc5
duongngockhanh <khanhduongsc@gmail.com>
2023-03-27 23:19:26 +0700

    revise yolov5 03

---
+++ yolov5_custom/detect.sh
@@ -0,0 +1,6 @@
+python detect.py --weights a2_weights/vehicle_v5n.pt --source a1_video_demo/23seconds.mp4
+
+
+# video 08: Detect long lane and short lane - lane_03.pt
+# video 10: Detect only short long          - lane_02.pt
+# video 12: Detect many object              - manyobj_02.pt
